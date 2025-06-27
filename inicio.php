dd07117e345d2129d30cc4f017e4611ed5772fa8
FantasmeiroFantasma <163547571+GustaioRocodaio@users.noreply.github.com>
2025-06-27 10:39:27 -0300

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masmorra do Geek</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+<div class="partedotopo">
+<h1>Masmorra do Geek</h1>
+
+<a href="inicio.php"><h2>inicio</h2></a>
+
+
+</div>
+
+
+
+</header>
+
+<h1>Figures em Destaque:</h1>
+
+<div id="figures">
+<div class="vecna">
+
+    <img src="asstes/vecna.webp" alt="" height="300px " width="300">
+    <h2>Vecna D&D figure 15cm</h2>
+</div>
+
+<div class="leon">
+
+    <img src="asstes/re4leon.webp" alt="" height="300px " width="300">
+    <h2>Vecna D&D figure 15cm</h2>
+</div>
+
+
+
+</div>
+
+
+    
+</body>
+</html>
