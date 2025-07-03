3af0ad02bc13b3361d173a0c26bb7d421a0a2fad
FantasmeiroFantasma <163547571+GustaioRocodaio@users.noreply.github.com>
2025-07-03 12:26:43 -0300

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pr-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masmorra do Geek</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+    <link rel="stylesheet" href="personaliza.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+  </head>
 <body>
 
 <header>
@@ -19,26 +24,64 @@
 
 
 
+
+
+
+
+
+
 </header>
+
+
+<h1>Aqui você encontra estatuetas para todos os seus gostos, desde animes, á séries, filmes e jogos.</h1>
+
+
+
+<div class="banner"><img src="asstes/Masmorra do Geek.png" alt="" height="240px" width="1513px"></div>
+
+
 
 <h1>Figures em Destaque:</h1>
 
-<div id="figures">
-<div class="vecna">
+<div class="figures">
+  <div class="vecna">
 
-    <img src="asstes/vecna.webp" alt="" height="300px " width="300">
-    <h2>Vecna D&D figure 15cm</h2>
+    <img src="asstes/vecna.webp" alt="" height="260px " width="225">
+    <h2>Vecna, Dungeons & Dragons Figure</h2>
+   </div>
+
+  <div class="leon">
+
+    <img src="asstes/re4leon.jpg" alt="" height="260px " width="225"     >
+    <h2>Leon, Resident Evil 4 Remake Action Figure</h2>
+   </div>
+
+
+   <div class="komi">
+
+    <img src="asstes/komi.webp" alt="" height="260px " width="225">
+    <h2>Komi-san, Komi-san wa, Komyushou desu Figure </h2>
+   </div>
+
+   <div class="alice">
+
+<img src="asstes/alice.webp" alt="" height="260px " width="225">
+<h2>Alice e o Gato de Cheshire, Alice Madness Returns Action Figure</h2>
 </div>
 
-<div class="leon">
-
-    <img src="asstes/re4leon.webp" alt="" height="300px " width="300">
-    <h2>Vecna D&D figure 15cm</h2>
-</div>
-
 
 
 </div>
+
+
+
+
+
+
+
+
+
+
 
 
     
