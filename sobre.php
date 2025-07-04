9e58671af76eb8167f32fb4a263355c0a161a46e
FantasmeiroFantasma <163547571+GustaioRocodaio@users.noreply.github.com>
2025-07-04 07:58:58 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre a MG</title>
+    <link rel="stylesheet" href="personaliza.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+
+
+</head>
+<body>
+    
+<header>
+<div class="partedotopo">
+<h1>Masmorra do Geek</h1>
+
+<a href="inicio.php"><h2>inicio</h2></a>
+
+
+</div>
+
+<h1>Fundada em 2020 em Curitiba, somos uma empresa que trazemos figuras de acção(action figures) para o Brasil para que fique perfeita na sua casa. Todas elas são originais e da mais alta qualidade. Temos contratos com empresas como a Bandai, Hot Toys, Iron Studios, Hasbro, Sideshow e muitas outras.
+
+
+
+
+</h1>
+
+
+<footer>
+        <div class="footer-content">
+            <div class="footer-links">
+            <a href="inicio.php">Home</a>
+
+
+            <a href="sobre.php">Sobre</a>
+                <a href="#">Contato</a>
+            </div>
+            <div class="copyright">
+                © 2023 Masmorra do Geek. Todos os direitos reservados.
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+</html>
